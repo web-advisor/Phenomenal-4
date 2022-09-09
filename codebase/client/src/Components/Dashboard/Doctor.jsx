37fb0d60d87c8eaed8a05856b9044c95c14f19d2
web-advisor <sinnaamar68@gmail.com>
2022-09-09 14:24:55 +0530

    Line

--- codebase/client/src/Components/Dashboard/Doctor.jsx
+++ codebase/client/src/Components/Dashboard/Doctor.jsx
@@ -6,7 +6,7 @@ function Doctor(props) {
     //  e.preventDefault();
     const pToken = `JWT ${localStorage.getItem("patientToken")}`;
     try {
-      const url = `http://localhost:5000/patient/appointments/create/${props.slug}`
+      const url = `https://fast-eyrie-20747.herokuapp.com/patient/appointments/create/${props.slug}`
       const response = await fetch(url, {
         method: "POST",
         headers: {
